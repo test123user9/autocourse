b17471cdb1135fb0251539ca711153ed92b36c9c
Hanna <annah.klymenko@gmail.com>
2022-08-04 08:38:36 +1000

    Adding 13 homework

--- Homework_13/worker.py
+++ Homework_13/worker.py
@@ -12,6 +12,14 @@ class Worker:
     @vacation_title
     @vacation_type_header
     def vacation_request_pattern(self, vacation_type: str, from_date: date = date.today(), to_date: date = date.today()):
+        """
+        The function prints vacation request depending on vacation type and from/to dates
+
+        :param vacation_type: Vacation type in type str. Allowed values: vacation, sick leave, day off
+        :param from_date: the first vacation day in type date(yyyy, mm, dd)
+        :param to_date: the last vacation day in type date(yyyy, mm, dd)
+        :return: None
+        """
         if from_date > to_date:
             raise InvalidDatePeriod(from_date)
         if from_date < date.today():
