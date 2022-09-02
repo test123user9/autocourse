13340c779f23af33a026e6e36df3212356f01312
Hanna <annah.klymenko@gmail.com>
2022-09-02 19:12:04 +1000

    Adding 20 homework

--- Homework_20/configuration.py
+++ Homework_20/configuration.py
@@ -1,11 +1,13 @@
 BASE_URL = 'https://petstore3.swagger.io/api/v3'
 ABOUT_PET = '/pet'
-FIND_BY_STATUS = '/findByStatus?status='
-
-# get
-AVAILABLE = f'{BASE_URL}{ABOUT_PET}{FIND_BY_STATUS}available'
-PENDING = f'{BASE_URL}{ABOUT_PET}{FIND_BY_STATUS}pending'
-SOLD = f'{BASE_URL}{ABOUT_PET}{FIND_BY_STATUS}sold'
+FIND_BY_STATUS = '/findByStatus'
 
 # post/put
 PET = f'{BASE_URL}{ABOUT_PET}'
+
+# get
+AVAILABLE = f'{PET}{FIND_BY_STATUS}?status=available'
+PENDING = f'{PET}{FIND_BY_STATUS}?status=pending'
+SOLD = f'{PET}{FIND_BY_STATUS}?status=sold'
+
+
